4299267abec042c6b54ece29ae93e2bfb7b2b265
Fernando J <ferb7o2@F-MacBook-Pro.local>
2023-01-08 03:12:11 -0600

    set up and deployed to serverless framework

---
+++ handler.js
@@ -0,0 +1,150 @@
+const serverless = require("serverless-http");
+const express = require("express");
+
+const dotenv = require("dotenv");
+const connectDB = require("./actions/dbConn");
+const { default: mongoose } = require("mongoose");
+
+dotenv.config();
+const app = express();
+
+Array.prototype.randomSong = function () {
+	return this[Math.floor(Math.random() * this.length)];
+};
+
+//connect to MongoDB
+connectDB();
+
+const Song = require("./models/song");
+const Album = require("./models/album");
+
+app.get("/", async (req, res) => {
+	try {
+		const songs = await Song.find();
+		console.log(songs);
+		res.json(songs);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.get("/test", async (req, res) => {
+	res.json("Hello from test!");
+});
+
+//GET all albums of $ARTIST
+app.get("/:artist/album", async (req, res) => {
+	const { artist } = req.params;
+	try {
+		const albums = await Album.find({ artist: artist });
+
+		return res.json(albums);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+//GET $ALBUM of $ARTIST
+app.get("/:artist/album/:album", async (req, res) => {
+	const { artist, album } = req.params;
+
+	try {
+		const albums = await Album.find({ artist: artist, name: album });
+		return res.json(albums);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+//GET all songs of $ALBUM of $ARTIST
+app.get("/:artist/album/:album/song", async (req, res) => {
+	const { artist, album } = req.params;
+
+	try {
+		const songList = await Album.find({ artist: artist, name: album });
+		const songs = await Song.find({
+			_id: { $in: songList[0].songs },
+		});
+		return res.json(songs);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+//GET random out of all songs of $ALBUM of $ARTIST
+app.get("/:artist/album/:album/song/random", async (req, res) => {
+	const { artist, album } = req.params;
+
+	try {
+		const songList = await Album.find({ artist: artist, name: album });
+		const songs = await Song.find({
+			_id: { $in: songList[0].songs },
+		});
+		return res.json(songs.randomSong());
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+//GET all songs of $ARTIST
+app.get("/:artist/song", async (req, res) => {
+	const { artist } = req.params;
+	let songList = [];
+
+	try {
+		const albums = await Album.find({ artist: artist });
+		albums.forEach((thing) => {
+			songList = songList.concat(thing.songs);
+		});
+
+		const songs = await Song.find({
+			_id: { $in: songList },
+		});
+		return res.json(songs);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+//GET $SONG by name
+app.get("/song/:song", async (req, res) => {
+	const { song } = req.params;
+
+	try {
+		const songRes = await Song.find({
+			name: song,
+		});
+		return res.json(songRes);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+//GET random out of all songs of $ARTIST
+app.get("/:artist/song/random", async (req, res) => {
+	const { artist } = req.params;
+	let songList = [];
+
+	try {
+		const albums = await Album.find({ artist: artist });
+		albums.forEach((thing) => {
+			songList = songList.concat(thing.songs);
+		});
+
+		const songs = await Song.find({
+			_id: { $in: songList },
+		});
+		return res.json(songs.randomSong());
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+// mongoose.connection.once("open", () => {
+// 	console.log("Connected to MongoDB");
+// 	app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+// });
+
+module.exports.handler = serverless(app, {
+	callbackWaitsForEmptyEventLoop: false,
+});
